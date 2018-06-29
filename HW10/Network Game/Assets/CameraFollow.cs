f12b8eec63f326dd94975307fb56d7da8eebd801
AzureSky <gg424463241@gmail.com>
2018-06-29 11:30:37 +0800

    Add HW10

---
+++ HW10/Network Game/Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followingTarget;       // The target this camera follows
+    public float dampTime = 0.2f;           // Approximate time for the camera to refocus
+
+    private Vector3 movingVelocity = Vector3.zero;
+
+    void FixedUpdate ()
+    {
+        if ( followingTarget != null ) {
+            transform.position = Vector3.SmoothDamp( transform.position, followingTarget.position, ref movingVelocity, dampTime );
+        }
+	}
+
+    public void SetTarget( Transform target )
+    {
+        followingTarget = target;
+    }
+}
---
+++ HW10/Network Game/Assets/TankNetworkController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TankNetworkController : NetworkBehaviour {
+
+    public GameObject cameraRigPrefab;
+    public Material localColor;
+
+    private CameraFollow cameraFollow;
+
+	public override void OnStartLocalPlayer()
+    {
+        // Create and Set camera
+        cameraFollow = Instantiate( cameraRigPrefab ).GetComponent<CameraFollow>();
+        cameraFollow.SetTarget( transform );
+
+        // Change the color of local player
+        foreach ( Transform child in transform ) {
+            if ( child.tag == "TankRenderer" ) {
+                for ( int i = 0; i < child.childCount; ++i ) {
+                    child.GetChild( i ).GetComponent<MeshRenderer>().materials[0].color = localColor.color;
+                }
+                break;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
